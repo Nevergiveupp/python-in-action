77523c6f32f80dfc846bf5e0911f2f60793c5ce6
WILLWAN3 <williams.wang@dbschenker.com>
2022-06-10 13:26:37 +0800

    add click movement

--- src/main/prince/moveCursor.py
+++ src/main/prince/moveCursor.py
@@ -7,9 +7,12 @@ def autoMove():
     while 1:
         # 5秒钟移动一次鼠标(移动鼠标时间可以根据自己需要设定)
         time.sleep(5)
-        pyautogui.moveTo(x=1500, y=random.randint(100, 900))
-        print("cursor moved to: x=")
+        x = 1500
+        y = random.randint(100, 900)
+        pyautogui.moveTo(x, y)
+        print("cursor moved to: x=" + str(x) + ", y=" + str(y))
+        pyautogui.click()
 
 
 if __name__ == "__main__":
-    autoMove();
+    autoMove()
